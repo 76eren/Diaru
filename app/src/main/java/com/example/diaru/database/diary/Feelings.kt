4a8968df3d75dc7d1ac507515b1cdd9d1120e6a0
AHsofty <e.basaran2004@gmail.com>
2024-02-05 20:06:29 +0100

    Feat: added enum with feelings

---
+++ app/src/main/java/com/example/diaru/database/diary/Feelings.kt
@@ -0,0 +1,10 @@
+package com.example.diaru.database.diary
+
+// This is an implementation of the feeling item in the Diary entity
+enum class Feelings {
+    AWFUl,
+    BAD,
+    NEUTRAL,
+    GOOD,
+    GREAT
+}
