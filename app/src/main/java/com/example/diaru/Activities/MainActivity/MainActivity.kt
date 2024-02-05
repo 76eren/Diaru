fdcbde5d72365cee84984c66e90eda27c5b0f2c7
AHsofty <e.basaran2004@gmail.com>
2024-02-05 23:02:46 +0100

    Feat: added a toolbar

--- app/src/main/java/com/example/diaru/Activities/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/diaru/Activities/MainActivity/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.example.diaru.Activities.MainActivity
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.Observer
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.diaru.Navbar.BottomNavigationBar
 import com.example.diaru.Navbar.Screen
-import com.example.diaru.database.diary.DiaryEntity
 import com.example.diaru.database.diary.DiaryViewModel
 import com.example.diaru.ui.theme.DiaruTheme
 
@@ -38,6 +43,7 @@ class MainActivity : ComponentActivity() {
 fun MyApp() {
     val navController = rememberNavController()
     Scaffold(
+        topBar = { Toolbar() },
         bottomBar = { BottomNavigationBar(navController) }
     ) { innerPadding ->
         NavHost(navController, startDestination = Screen.Home.route, Modifier.padding(innerPadding)) {
@@ -45,4 +51,26 @@ fun MyApp() {
             composable(Screen.Settings.route) { }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Toolbar() {
+    TopAppBar(
+        title = { Text("Actions", color = Color.White) },
+        actions = {
+            IconButton(onClick = { addBook() }) {
+                Icon(Icons.Filled.Add, contentDescription = "Add", tint = Color.White)
+            }
+        },
+        colors = androidx.compose.material3.TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Blue,
+            titleContentColor = Color.White,
+            actionIconContentColor = Color.White
+        )
+    )
+}
+
+fun addBook() {
+    // TODO: implement this
 }
