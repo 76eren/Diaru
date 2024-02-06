d84c410074ad6b34727930655a4d8f79eeac17c0
AHsofty <e.basaran2004@gmail.com>
2024-02-06 23:55:19 +0100

    Change: Changed the font for the diary input

--- app/src/main/java/com/example/diaru/Activities/CreateDiaryActivity/DiaryWrite.kt
+++ app/src/main/java/com/example/diaru/Activities/CreateDiaryActivity/DiaryWrite.kt
@@ -17,16 +17,27 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.diaru.ui.theme.notepadYellow
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
+import androidx.annotation.FontRes // If you use annotation for font resource IDs
+import com.example.diaru.R
 
 
 @Composable
 fun DiaryWriteEntry(diaryCreateViewModel: DiaryCreateViewModel) {
     var textState by remember { mutableStateOf(TextFieldValue("")) }
 
+    val customFontFamily = FontFamily(Font(R.font.beaty_diary))
+
     BasicTextField(
         value = textState,
         onValueChange = { textState = it },
-        textStyle = TextStyle(fontSize = 18.sp, color = Color.Black),
+        textStyle = TextStyle(fontSize = 26.sp, color = Color.Black, fontFamily = customFontFamily),
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
