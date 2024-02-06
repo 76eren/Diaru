2dfc62df6545097a2cd0d8285137083fa89132a6
AHsofty <e.basaran2004@gmail.com>
2024-02-06 01:49:55 +0100

    Change: changed colours

--- app/src/main/java/com/example/diaru/Activities/CreateDiaryActivity/Feelings.kt
+++ app/src/main/java/com/example/diaru/Activities/CreateDiaryActivity/Feelings.kt
@@ -1,73 +1,55 @@
 package com.example.diaru.Activities.CreateDiaryActivity
 
-import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.diaru.R
-import com.example.diaru.database.diary.DiaryViewModel
+import com.example.diaru.ui.theme.darkSkyBlue
 
 
 @Composable
 fun FeelingBar(createViewModel: DiaryCreateViewModel) {
     var feeling by remember { mutableStateOf(-1) }
 
-    val rainbowColors: List<Color> = listOf(
-        Color.Red, Color(0xFFFFA500), Color.Yellow, Color.Green, Color.Blue,
-        Color(0xFF4B0082), Color(0xFF8A2BE2)
-    )
+    val rainbowColors: List<Color> = listOf(Color.Red, Color(0xFFFFA500), Color.Yellow, Color.Green, Color.Blue, Color(0xFF4B0082), Color(0xFF8A2BE2))
 
-    val text = "How did you feel today?"
-
-    val coloredText = buildAnnotatedString {
-        text.forEachIndexed { index, char ->
-            val color = rainbowColors[index % rainbowColors.size]
-            withStyle(style = SpanStyle(color = color)) {
-                append(char.toString())
-            }
-        }
-    }
-
-    Column(
+    Column (
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    )
+    {
         Text(
-            text = coloredText,
+            text = "How did you feel today?",
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 8.dp, bottom = 90.dp, start = 8.dp, end = 8.dp),
+                .padding(top = 8.dp, bottom = 90.dp, start =8.dp, end = 8.dp),
             style = MaterialTheme.typography.displayLarge.copy(
                 fontWeight = FontWeight.Bold,
                 letterSpacing = 1.5.sp,
                 lineHeight = 48.sp
             ),
+            color = darkSkyBlue,
             textAlign = TextAlign.Center,
             textDecoration = TextDecoration.Underline
+
         )
 
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Row(modifier = Modifier
+            .fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+
             val images = listOf(
                 R.drawable.crying_lots,
                 R.drawable.crying,
@@ -85,8 +67,6 @@ fun FeelingBar(createViewModel: DiaryCreateViewModel) {
         }
     }
 }
-
-
 
 
 @Composable
